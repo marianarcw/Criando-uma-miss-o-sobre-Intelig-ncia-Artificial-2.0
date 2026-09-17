4e90322df77e2fe5d8511d767a01761a1dc75bf0
marianarcw <rachow.mariana@escola.pr.gov.br>
2026-09-17 10:51:14 -0300

    .

---
+++ script.js
@@ -0,0 +1,100 @@
+import { aleatorio, nome } from "./aleatorio.js";
+import { perguntas } from "./perguntas.js";
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function substituiNome() {
+    for (const pergunta of perguntas) {
+        pergunta.enunciado = pergunta.enunciado.replace(/você/gi, nome);
+    }
+}
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+
+    perguntaAtual = perguntas[atual];
+
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+    caixaAlternativas.textContent = "";
+
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+
+        botaoAlternativas.textContent = alternativa.texto;
+
+        botaoAlternativas.addEventListener("click", () => {
+            respostaSelecionada(alternativa);
+        });
+
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+
+    historiaFinal += afirmacoes + " ";
+
+    if (opcaoSelecionada.proxima !== undefined) {
+        atual = opcaoSelecionada.proxima;
+    } else {
+        atual++;
+    }
+
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = `Em 2049, ${nome}...`;
+
+    textoResultado.textContent = historiaFinal;
+
+    caixaAlternativas.textContent = "";
+
+    caixaResultado.classList.add("mostrar");
+}
+
+function iniciaJogo() {
+    atual = 0;
+    historiaFinal = "";
+
+    telaInicial.style.display = "none";
+
+    caixaResultado.classList.remove("mostrar");
+
+    mostraPergunta();
+}
+
+function jogaNovamente() {
+    atual = 0;
+    historiaFinal = "";
+
+    caixaResultado.classList.remove("mostrar");
+
+    mostraPergunta();
+}
+
+botaoIniciar.addEventListener("click", iniciaJogo);
+
+botaoJogarNovamente.addEventListener("click", jogaNovamente);
+
+substituiNome();
